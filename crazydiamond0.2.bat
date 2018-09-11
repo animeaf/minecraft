ce3e874db8ba08541e61d6c89d705e82ac9ec1b0
animeaf <zaxman18@gmail.com>
2018-09-11 19:14:50 +0200

    Update crazydiamond0.2.bat

--- crazydiamond0.2.bat
+++ crazydiamond0.2.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFDFBTQqhPXm2CaYgzO3o5P6IsnE+RuMlYc//07WDL+4X1kb2dIw0mGpUn4UFDxQ4
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSjk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJgZkkaHErTXA==
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQITOhpOVUSmNWq3CbsYiA==
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQITOhpOVUSmNWq3CbsYqKW7w+uSrE4VUfBf
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFDFBTQqhPXm2CaYgzO3o5P6IsnE+RuMlYc//07WDL+4X1lD0cZc502lmyYVfMA5IehO/a156+lJBpGWfLsOVsgLuREzH41M1ew==
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 title Crazy Diamond Launcher
 rem Version 0.2.0.1
