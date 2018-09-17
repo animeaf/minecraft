2a1e77f247b9ebda1d54b20d67edb31bd3f13bf2
animeaf <zaxman18@gmail.com>
2018-09-17 18:33:05 +0200

    Add files via upload

---
+++ Minecraft Launcher/Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Cargando...")
+        Try
+            Shell("minecraft.exe --workDir files_minecraft", 3)
+        Catch FileNotFound As System.IO.FileNotFoundException
+            Console.WriteLine()
+            Console.ForegroundColor = ConsoleColor.Red
+            Console.WriteLine("Error: minecraft.exe no encontrado.")
+            MsgBox("Error: Launcher original de Minecraft no encontrado.")
+        End Try
+
+    End Sub
+
+End Module
